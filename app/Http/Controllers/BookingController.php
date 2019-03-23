bb28e5fddb10b3836241e5100955304c0307c21f
marcella <marcellavanduyn@gmail.com>
2019-03-23 10:55:01 +0100

    BookingController.php
    StoreController.(Saves to DataBase)
    StoreData. (Timezone, and Error)
    GetData (Show data in the view)

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -4,88 +4,57 @@ namespace App\Http\Controllers;
 
 use App\booking;
 use Illuminate\Http\Request;
+use Auth;
+use Session;
+use Redirect;
 
 class BookingController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
+
+    public function storeData(Request $request) {
+        date_default_timezone_set('Europe/Amsterdam');
+        if (!request('start_booking')) {
+            $startBook = time();
+        } else {
+            if (strtotime(request('start_booking')) < time()) {
+                Session::put('error', "Error! You are trying to book an accomodation in the past!");
+                return view('home');
+            }
+            $startBook = strtotime(request('start_booking'));
+        }
+        if (!request('eind_booking')) {
+            $eindBook = time() + 604800;
+        } else {
+            if (strtotime(request('eind_booking')) < time()) {
+                Session::put('error', "Error! You are trying to book an accomodation with an end-time in the past!");
+                return view('home');
+            }
+            $eindBook = strtotime(request('eind_booking'));
+        }
+        if (!request('hoevaak')) {
+            $hoevaak = 1;
+        } else {
+            $hoevaak = request('hoevaak');
+        }
+        for ($i = 0; $i < $hoevaak; $i++) {
+            if ($i >= 1) {
+                $startBook = $startBook + 604800;
+                if ($startBook > $eindBook) {
+                    continue;
+                }
+            }
+            $nieuweBooking = new Booking();
+            $nieuweBooking->user_id = Auth::user()->id;
+            $nieuweBooking->start_booking = $startBook + 3600;
+            $nieuweBooking->eind_booking = $eindBook + 3600;
+            $nieuweBooking->save();
+        }
+        Session::put('status', "Gesaved!");
+        return Redirect::route('home');
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-        $nieuweBooking = new Booking();
-        $nieuweBooking->Aankomst;
-        $nieuweBooking->save();
-        $booking = Booking::where(‘userid’, Auth::user()->id)->get();
-        return view(‘home’, [‘booking’ => $booking]);
-    }
-
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\booking  $booking
-     * @return \Illuminate\Http\Response
-     */
-    public function show(booking $booking)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\booking  $booking
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(booking $booking)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\booking  $booking
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, booking $booking)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\booking  $booking
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(booking $booking)
-    {
-        //
+    public function getData() {
+        $booking = Booking::where('user_id', Auth::user()->id)->get();
+        return view('test', ['booking' => $booking]);
     }
 }
